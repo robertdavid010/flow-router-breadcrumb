2c3fdae6ef47fb1d7cbc9ba4f13f8d3beb38c753
robert <robert@monetsu.com>
2017-02-02 23:31:55 +0100

    Rework cakeCrumb logic

--- flow-router-breadcrumb.js
+++ flow-router-breadcrumb.js
@@ -10,7 +10,7 @@
  * and other properties in the new configuration options.
  *
  */
-var Breadcrumb = {};
+ var Breadcrumb = {};
 
 var data = {}; // routes are registered here on initialization
 var dataArray = []; // this is the data to render for breadcrumbs
@@ -23,183 +23,152 @@ var inited = false; // flag for initialization
 
 // Configs
 var levelsConfig = {
-    "listLevel": {
-        depth: 1,
-        order: 1,
-        priority: 1
-    },
-    "detailsLevel": {
-        depth: 2,
-        order: 1,
-        priority: 2
-    },
-    "inputLevel": {
-        depth: 1,
-        order: 1,
-        priority: 3
-    }
+  "homeLevel" : {
+    depth: 1,
+    order: 1,
+    priority: 0
+  },
+	"listsLevel": {
+		depth: 1,
+		order: 1,
+		priority: 1
+	},
+	"detailsLevel": {
+		depth: 2,
+		order: 1,
+		priority: 2
+	},
+	"inputsLevel": {
+		depth: 1,
+		order: 1,
+		priority: 3
+	}
 };
 
 Breadcrumb.Config = {
-    homeRoute: "home",
-    maxLevels: 4,
-    levelsConfig: levelsConfig
+	homeRoute: "home",
+	maxLevels: 4,
+	levelsConfig: levelsConfig
 };
 
-// Initialize for cakeCumbs
+/* Initialize cakeCrumbs ************************* */
 Breadcrumb.initialize = function () {
     // Session.set("cakeCrumbs", {});
-    // console.log("we are initializing cakeCrumbs");
-    // console.log(route);
     var r = FlowRouter.getRouteName();
     var homeRoute = Breadcrumb.Config.homeRoute;
     if (r != homeRoute) {
-        // this is not home, so we add the first crumb as the home route
+        // this is not homeRoute, so we add the first crumb as the homeRoute
         var routePath = data[homeRoute];
         cakeDataArray[0] = {homeLevel: [routePath]};
-    }
-    inited = true; // We have initialized the breadcrumb package
-};
+      }
+    inited = true;
+  };
 
-// Register
+/* Register route data ************************* */
 Breadcrumb.register = function (route) {
 
-    route.options.breadcrumb.title = route.options.breadcrumb.title || 'No Title';
-
-    data[route.name] = route;
-
-    if (!inited) {
-        Breadcrumb.initialize();
-    }
+	route.options.breadcrumb.title = route.options.breadcrumb.title || 'No Title';
+	data[route.name] = route;
 
 };
 
+// Generate the crumb for cakeCrumbs
 Breadcrumb.generate = function (paramsAll) {
-    // This will generate the cake crumbs
+  // This method will generate the cakeCrumbs data array
 
-    // var routeName = FlowRouter.getRouteName(); // Why not get this from paramsAll?
-    var routeName = paramsAll.route.name;
-    // console.log("generating crumb: routeName");
-    // console.log(routeName);
+	if (!inited) {
+		Breadcrumb.initialize();
+	}
 
-    // Get the necessary necessary data
-    var getRouter = data[routeName]; // Get registered route info
-    var bCrumb = getRouter && getRouter.options && getRouter.options.breadcrumb || null;
-    // var level = getRouter && getRouter.options && getRouter.options.breadcrumb && getRouter.options.breadcrumb.level || null;
-    var level = bCrumb && bCrumb.level || null
-    // var priority = Breadcrumb.Config.levelsConfig[routeName] && Breadcrumb.Config.levelsConfig[routeName].priority;
-    var crumbs = cakeDataArray;
+  var crumbs = cakeDataArray; // This is the current set of crumbs which we will update
+  var routeName = paramsAll.route.name;
+  var getRouter = data[routeName]; // Get registered route data
+  var updater = {};
 
-    // Add the passed route params
-    // TODO: Isn't this just a simple merge? It just updates the registered data
-    // This is to add params etc?
-    for (var p in paramsAll) {
-        if (p && getRouter) {getRouter[p] = paramsAll[p];}
+  // Update route data to newest, if existing
+  for (var p in paramsAll) { 
+  	if (p && getRouter) {getRouter[p] = paramsAll[p];}
+  }
+
+  // Parse route and config for relevant data
+  var bCrumb = getRouter && getRouter.options && getRouter.options.breadcrumb || null;
+  var level = bCrumb && bCrumb.level || null; // What if there is no level? Assume highest?
+  var levelCfg = Breadcrumb.Config.levelsConfig[level] || null;
+  var priority = levelCfg.priority;
+
+  // Start checking the existing crumbs for matching levels and routes
+  var levelMatch = crumbs.findIndex(function (e, i) {
+    return (e[level] || Array.isArray(e[level]));
+  });
+
+  if (levelMatch < 0) { // '0' must return false
+
+
+    if (priority < crumbs.length) {
+      // This level has higher priority, override lower priority
+      cakeDataArray.splice(priority)
     }
 
-    // TODO: We can change this whole matching logic?
-    // We just trim and place based on priority?
-    // Initialize matched value
-    var matched = -1;
-    var arr = [];
+		updater[level] = [getRouter];
+    cakeDataArray.push(updater);
 
-    // This hecks for existing level in crumbs
-    crumbs.forEach(function (e, i) {
-        if (e[level] || Array.isArray(e[level])) {
-            matched =  i;
-        }
+  } else { // There is an existing crumb level
+
+    var lvlCrumbs = crumbs[levelMatch][level];
+
+    var routeMatch = lvlCrumbs.findIndex(function (e, i) {
+      return e.name === getRouter.name;
     });
 
-    if (matched && matched > 0) {
-        // Get the existing array of items currently set for this level
-        console.log("this level exists");
-        console.log(matched);
-        arr = cakeDataArray && cakeDataArray[matched] && cakeDataArray[matched][level] || [];
+    if (routeMatch >= 0) { // '0' must return true
+      lvlCrumbs.splice(routeMatch);
     }
 
-    var isInArray = -1;
-    // Go through the array (if not empty) to check if this route is in the array
-    for (var i = arr.length - 1; i >= 0; i--) {
-        var nm = arr[i];
-        if (nm.name === getRouter.name) {
-            isInArray = i;
-        }
-    }
+    lvlCrumbs.push(getRouter)
+    updater[level] = lvlCrumbs;
 
-    if (isInArray >= 0) {
-        // var len = arr.length - (isInArray);
-        arr.splice(isInArray); // Adjust the level array to put this route as last crumb
-        console.log("we are modding the array")
-        console.log(arr);
-    }
+    cakeDataArray.splice(levelMatch + 1);
+    cakeDataArray[levelMatch] = updater; 
 
-    arr.push(getRouter); // Add the level array item
-    console.log(arr);
+  }
 
-    // TODO: this is where we can sort priority
-    // TODO: We need to trim cumbs to the current lowest level
-    var updater = {};
-
-    if (level) {
-        updater[level] = arr;
-    }
-
-    var homeRoute = Breadcrumb.Config.homeRoute;
-    if (getRouter && getRouter.name === homeRoute) {
-        // This should only ever be 0
-        cakeDataArray = [];
-        cakeDataArray[matched] = updater;
-    } else {
-        if (matched > 0) {
-            var len = cakeDataArray.length - (matched + 1);
-            cakeDataArray.splice(matched); // removes level to be overwritten
-        }
-        cakeDataArray.push(updater);
-    }
-
-    // Session.set("sessCrumbs", cakeDataArray);
-    console.log("checking cakeDataArray");
-    console.log(cakeDataArray);
-
-    return false;
+  console.log("checking cakeDataArray");
+  console.log(cakeDataArray);
+  Breadcrumb.renderCake(); // This forces re-render, which was not always happening
 
 }; // END Breadcrumb.generate()
 
 // Dynamically generate and update crumb data
 Breadcrumb.renderCake = function () {
 
-    crumbArr = []; // Clear data array
-    var routeName = routeName || FlowRouter.getRouteName();
-    // var cDataArr = Session.get("sessCrumbs");
-    // console.log("this is the session object");
-    // console.log(cDataArr);
+  crumbArr = []; // Clear data array
+  var routeName = routeName || FlowRouter.getRouteName();
 
-    // Loop through the level objects
-    for (var i = 0; i < cakeDataArray.length; i++) {
-        var ob = cakeDataArray[i];
-        var elem = ob[Object.keys(ob)[0]];
+  // Loop through the level objects, and transpose into list for rendering
+  for (var i = 0; i < cakeDataArray.length; i++) {
+  	var ob = cakeDataArray[i];
+  	var elem = ob[Object.keys(ob)[0]]; // Get the first key of the associative elements
 
-        // console.log("object looper"); // Will show reactive retriggers
-        // console.log(cakeDataArray.length);
+  	// Assemble the final crumb obj
+    var routes = elem.forEach(function (el, ix, arr) {
+    	var obj = {};
 
-        // Loop in the array of nested object
-        // console.log("what is the elem?");
-        // console.log(elem);
-        var routes = elem.forEach(function (el, ix, arr) {
-            var obj = {};
-            obj.url = FlowRouter.path(el.name, el.params, el.queryParams);
-            obj.title = el.options.breadcrumb.title;
-            if (routeName === el.name) {
-                obj.activeClass = "active";
-            } else {
-                obj.activeClass = "";
-            }
-            crumbArr.push(obj); // TODO: This causes the helper re-trigger
-        });
+    	obj.url = FlowRouter.path(el.name, (el.params || null), (el.queryParams || null));
+    	obj.title = el.options.breadcrumb.title;
 
-    };
+    	if (routeName === el.name) {
+    		obj.activeClass = "active";
+    	} else {
+    		obj.activeClass = "";
+    	}
 
-    return crumbArr;
+      crumbArr.push(obj);
+
+    });
+
+  };
+
+  return crumbArr;
 
 };
 
@@ -219,22 +188,22 @@ Breadcrumb.renderBread = function (routeName) {
 
     // Push data
     dataArray.push({
-        url: url,
-        title: getRouter.options.breadcrumb.title,
-        activeClass: 'active'
+    	url: url,
+    	title: getRouter.options.breadcrumb.title,
+    	activeClass: 'active'
     });
 
     // Check parent
     if (getRouter.options.breadcrumb.parent) {
-        getParent(getRouter.options.breadcrumb.parent)
+    	getParent(getRouter.options.breadcrumb.parent)
     }
 
     return dataArray.reverse();
-};
+  };
 
 // Get parent router
 var getParent = function (route) {
-    var getRouter = data[route];
+	var getRouter = data[route];
 
     // Generate route url
     var paramAndQuery = genParamAndQuery(getRouter.options.breadcrumb);
@@ -242,18 +211,18 @@ var getParent = function (route) {
 
     // Push data
     dataArray.push({
-        url: url,
-        title: getRouter.options.breadcrumb.title,
-        activeClass: ''
+    	url: url,
+    	title: getRouter.options.breadcrumb.title,
+    	activeClass: ''
     });
 
     // Check parent parent
     if (getRouter.options.breadcrumb.parent) {
-        getParent(getRouter.options.breadcrumb.parent)
+    	getParent(getRouter.options.breadcrumb.parent)
     }
 
     return false;
-};
+  };
 
 // Generate param and query
 var genParamAndQuery = function (breadcrumb) {
@@ -263,49 +232,50 @@ var genParamAndQuery = function (breadcrumb) {
 
     var params = {};
     _.each(breadcrumb.params, function (o) {
-        params[o] = FlowRouter.getParam(o);
+    	params[o] = FlowRouter.getParam(o);
     });
     var queryParams = {};
     _.each(breadcrumb.queryParams, function (o) {
-        queryParams[o] = FlowRouter.getQueryParam(o);
+    	queryParams[o] = FlowRouter.getQueryParam(o);
     });
 
     return {params: params, queryParams: queryParams};
-};
+  };
 
 /**
  * Register to flow router
  */
-FlowRouter.onRouteRegister(function (route) {
+ FlowRouter.onRouteRegister(function (route) {
     // This puts all route breadcrumb definitions in the data[] array;
     if (route.options.breadcrumb) {
-        Breadcrumb.register(route);
+    	Breadcrumb.register(route);
     }
-});
+  });
 
 /**
  * Track route changes
  */
-if (Meteor.isClient) {
-    Meteor.startup(function() {
-        Tracker.autorun(function(c) {
-          FlowRouter.watchPathChange();
-          if (FlowRouter._initialized) {
+ if (Meteor.isClient) {
+ 	Meteor.startup(function() {
+ 		Tracker.autorun(function(c) {
+ 			FlowRouter.watchPathChange();
+ 			if (FlowRouter._initialized) {
             // Trigger the dynamic crumb generation on route change...
+            console.log("triggered by route");
             var paramsAll = FlowRouter.current();
             Breadcrumb.generate(paramsAll);
           }
         });
-    });
-}
+ 	});
+ }
 
 /**
  * Global Template helpers
  */
-Template.registerHelper('breadcrumb', function () {
-    return Breadcrumb.renderBread();
-});
+ Template.registerHelper('breadcrumb', function () {
+ 	return Breadcrumb.renderBread();
+ });
 
-Template.registerHelper('cakecrumb', function () {
-    return Breadcrumb.renderCake();
-});
+ Template.registerHelper('cakecrumb', function () {
+ 	return Breadcrumb.renderCake();
+ });
